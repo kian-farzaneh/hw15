dfde7f8e1122837c186f98792270fefc2b053e90
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-01-27 12:01:48 +0330

    create form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import './form.css'
 
 function Form() {
     return <>
         <div className="formContainer">
-            <h4>Lorem, ipsum dolor.</h4>
-            <h4>Lorem ipsum dolor sit amet consectetur.</h4>
+            <div className="form-header">
+                <h4>Lorem, ipsum dolor.</h4>
+                <h4>Lorem ipsum dolor sit amet consectetur.</h4>
+            </div>
+            <div className="form-inputs">
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <div className="form-button">
+                <button></button>
+            </div>
         </div>
     </>;
 };
--- src/components/landingPageComponents/LandingPageComponents.jsx
+++ src/components/landingPageComponents/LandingPageComponents.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../header/Header";
 import Form from "../Form/Form";
+import './landingPageComponents.css'
 
 export default function LandingPageComponents() {
     return(
-        <Header />,
-        <Form />
+        <div className="landingContainer">
+            <Header />,
+            <Form />
+        </div>
     )
 };
