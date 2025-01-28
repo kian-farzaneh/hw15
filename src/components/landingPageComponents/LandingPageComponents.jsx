975c0b5abe74b5723b64a228e59e62c48b47a1c1
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-01-28 09:37:55 +0330

    create offer and benefits section

--- src/components/landingPageComponents/LandingPageComponents.jsx
+++ src/components/landingPageComponents/LandingPageComponents.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "../header/Header";
 import Form from "../Form/Form";
+import Offer from "../Offer/Offer";
 import './landingPageComponents.css'
 
 export default function LandingPageComponents() {
-    return(
+    return (
         <div className="landingContainer">
-            <Header />,
-            <Form />
+            <div className="header">
+                <Header />
+            </div>
+            <div className="main">
+                <Form />
+                <Offer />
+            </div>
         </div>
     )
 };
