631de92b628177250f58d1c81040c54bd44ddccb
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-01-28 13:30:59 +0330

    create socialproof

--- src/components/landingPageComponents/LandingPageComponents.jsx
+++ src/components/landingPageComponents/LandingPageComponents.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../header/Header";
 import Form from "../Form/Form";
 import Offer from "../Offer/Offer";
+import SocialProof from "../SocialProof/SocialProof";
 import './landingPageComponents.css'
 
 export default function LandingPageComponents() {
@@ -13,6 +14,9 @@ export default function LandingPageComponents() {
             <div className="main">
                 <Form />
                 <Offer />
+            </div>
+            <div className="socialProof">
+                <SocialProof />
             </div>
         </div>
     )
